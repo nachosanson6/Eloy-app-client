6b5f7a2c898bb7d3e481c5255f1398cbe050ad74
nachosanson6 <nachosanson6@gmail.com>
2023-12-31 02:18:33 +0100

    q

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,18 +1,42 @@
 import React, { useState } from 'react';
 import AwesomeSlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/styles.css';
+import './Carousel.css';
 
-const Carousel = () => {
-    const [photos, setPhotos] = useState([]);
-    
+const Carousel = ({ photos }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-return (
-        <AwesomeSlider>
-            <div data-src='https://res.cloudinary.com/dpkuxyam6/image/upload/v1703670416/btnfsa96mp27lkgt6yp9.png'/>
-            <div data-src='https://res.cloudinary.com/dpkuxyam6/image/upload/v1701968049/sh23xrisdeeu6rg20sja.jpg'/>
-            <div data-src='https://res.cloudinary.com/dpkuxyam6/image/upload/v1703674206/ire5oyz5bg9fy4dgvqx0.png'/>
-        </AwesomeSlider> 
-   )
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % photos.length);
+  };
 
-}
-   export default Carousel;
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? photos.length - 1 : prevIndex - 1
+    );
+  };
+
+
+
+  return (
+    <div className="carousel">
+      <button onClick={prevSlide}>Previous</button>
+      <div className="slide-container">
+        {photos.map((image, index) => (
+            <>
+          <div
+            key={index}
+            className={`slide ${index === currentIndex ? 'active' : ''}`}
+          >
+            <img src={image}  alt={`Slide ${index + 1}`} />
+          </div>
+          </>
+        ))}
+        
+      </div>
+      <button onClick={nextSlide}>Next</button>
+    </div>
+  );
+};
+
+export default Carousel;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,17 +1,23 @@
 import { Container, Row, Col } from "react-bootstrap"
-import PicturesForm from "../../components/PicturesForm/PicturesForm"
-import SculptureForm from "../../components/SculpturesForm/SculptureForm"
-import JewelryForm from "../../components/JewelryForm/JewelryForm"
 import Carousel from "../../components/Carousel/Carousel"
+import { useEffect, useState } from "react";
+import pictureService from "../../services/picture.services";
 
 const HomePage = () => {
+
+    const images = [
+        'https://res.cloudinary.com/dpkuxyam6/image/upload/v1701968049/sh23xrisdeeu6rg20sja.jpg',
+        'https://res.cloudinary.com/dpkuxyam6/image/upload/v1703670416/btnfsa96mp27lkgt6yp9.png',
+        'https://res.cloudinary.com/dpkuxyam6/image/upload/v1703697460/psfjc4pn5xjyfbxmshkf.jpg',
+      ];
+
     return (
         <>
             <Container>
                 <Row>
                     <Col md={{ offset: 3, span: 6 }}>
                         <h1>Home Page</h1>
-                        <Carousel />
+                        <Carousel photos={images}/>
                     </Col>
                 </Row>
             </Container>
