1b6c95cd5410e68f11fbbd354e346b51101bec1e
nachosanson6 <nachosanson6@gmail.com>
2024-01-18 12:09:50 +0100

    w

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,58 +1,88 @@
-import { useEffect, useState } from "react";
-import { Carousel, Col, Row } from "react-bootstrap";
+import React, { useEffect, useState, useRef } from "react";
+import { Carousel } from "react-bootstrap";
 import Loading from "../Loading/Loading";
-import allProductsService from "../../services/allProducts.services";
-import "./Carousel.css"
+import "./Carousel.css";
 
-const CarouselComponent = ({photos}) => {
-
+const CarouselComponent = ({ photos }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  
+  const mainImageRef = useRef(null);
+  const [isZoomed, setIsZoomed] = useState(false);
+
   const showImage = (index) => {
     setCurrentIndex(index);
   };
 
   const nextImage = () => {
-    setCurrentIndex((currentIndex + 1) % photos.length);
+    if (!isZoomed) {
+      setCurrentIndex((currentIndex + 1) % photos.length);
+    }
   };
 
   const prevImage = () => {
     setCurrentIndex((currentIndex - 1 + photos.length) % photos.length);
   };
 
+  const handleZoom = () => {
+    const mainImage = mainImageRef.current;
+  
+    if (mainImage) {
+      if (mainImage.classList.contains('zoomed')) {
+        mainImage.classList.remove('zoomed');
+        setIsZoomed(false);
+      } else {
+        mainImage.classList.add('zoomed');
+        setIsZoomed(true);
+      }
+    }
+  };
+
   useEffect(() => {
     const interval = setInterval(() => {
       nextImage();
-    }, 10000);
-    return () => clearInterval(interval);
+    }, 5000);
+
+    return () => {
+      clearInterval(interval);
+    };
   }, [currentIndex]);
 
-    if (!photos) {
-      return (
-          <Loading />
-      )
+  if (!photos) {
+    return <Loading />;
   }
-  return(
-    <div id="carousel-container" >
+
+  return (
+    <div id="carousel-container">
       <div id="nav-container">
-      <button id="prev-btn" className="nav-btn" onClick={prevImage}>&lt;</button>
-     
-    <img id="main-image" src={photos[currentIndex]} alt={`Imagen ${currentIndex + 1}`} />
-    <button id="next-btn" className="nav-btn" onClick={nextImage}>&gt;</button>
+        <button id="prev-btn" className="nav-btn" onClick={prevImage}>
+          &lt;
+        </button>
+
+        <img
+          id="main-image"
+          ref={mainImageRef}
+          src={photos[currentIndex]}
+          alt={`Imagen ${currentIndex + 1}`}
+          onClick={handleZoom} 
+        />
+
+        <button id="next-btn" className="nav-btn" onClick={nextImage}>
+          &gt;
+        </button>
       </div>
-    <div id="thumbnail-container">
+
+      <div id="thumbnail-container">
         {photos.map((image, i) => (
           <div
             key={i}
-            className={`thumbnail ${i === currentIndex ? 'active' : ''}`}
+            className={`thumbnail ${i === currentIndex ? "active" : ""}`}
             onClick={() => showImage(i)}
           >
             <img src={image} alt={`Thumbnail ${i + 1}`} />
           </div>
         ))}
       </div>
-      
-  </div>
-  )
-}
-export default CarouselComponent
+    </div>
+  );
+};
+
+export default CarouselComponent;
