8824c98afe6c000c240d67f4f573a3d261e144e0
nachosanson6 <nachosanson6@gmail.com>
2024-01-29 22:45:51 +0100

    q

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Claim from './components/Claim/Claim'
 import Footer from './components/Footer/Footer'
 import Navigation from './components/Navigation/Navigation'
 import AppRoutes from './routes/AppRoutes'
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Navigation />
+      <Claim />
       <div className='pt-5 '>
         <AppRoutes />
       </div>
---
+++ src/components/Claim/Claim.jsx
@@ -0,0 +1,18 @@
+import { Container } from "react-bootstrap"
+import "./Claim.css"
+
+
+const Claim = () => {
+    return (
+        <div className="claim">
+            <h1>Descubre El Mundo De Eloy</h1>
+            <h4>Donde la Inspiración se Encuentra con la Creatividad</h4>
+            <div className="claimButtons">
+                <button>Ver pinturas</button>
+                <button>Ver esculturas</button>
+                <button>Ver joyería</button>
+            </div>
+        </div>
+    )
+}
+export default Claim
