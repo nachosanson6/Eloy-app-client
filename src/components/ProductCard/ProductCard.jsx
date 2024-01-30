1b8e53836de83cb6d3c21f47b422ce797473f7ec
nachosanson6 <nachosanson6@gmail.com>
2024-01-30 10:03:44 +0100

    d

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -2,15 +2,34 @@ import { Link } from "react-router-dom"
 import "./ProductCard.css"
 
 
-const ProductCard = ({ name, photo, _id, type }) => {
-    return (
-        <div className="card">
+const ProductCard = ({ name, photo, _id, product, height, width, materials, colors }) => {
+    let chipContent;
+    switch (product) {
+        case 'Pictures':
+            chipContent = 'Pintura';
+            break;
+        case 'Sculptures':
+            chipContent = 'Escultura';
+            break;
+        case 'Jewelry':
+            chipContent = 'Joyería';
+            break;
+        default:
+            chipContent = 'Tipo Desconocido';
+            break;
+    }
 
-            <Link to={`/productDetails/${_id}`} className="imageLink">
-                <img src={photo} alt="" className="image" />
-                <p className="chip">{type}</p>
-            </Link>
-        </div>
+    return (
+        <>
+            <div className="card">
+                <Link to={`/productDetails/${_id}`} className="imageLink">
+                    <img src={photo} alt="" className="image" />
+                    <p className="chip">{chipContent}</p>
+                </Link>
+            </div>
+            <h4 className="productName">{name}</h4>
+            <p className="productDetails" >{height} x {width}</p>
+        </>
     )
 }
 export default ProductCard
