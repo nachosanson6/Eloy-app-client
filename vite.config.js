b328c0e9ab822d1321dcd385d8a3c230d497cb5d
nachosanson6 <nachosanson6@gmail.com>
2024-01-31 12:21:10 +0100

    t

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import ViteFontsPlugin from 'vite-plugin-fonts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    ViteFontsPlugin({
-      google: {
-        families: [
-          {
-            name: 'Familjen Grotesk',
-            styles: 'ital,wght@0,400;0,700;1,400;1,700',
-          },
-        ],
-      },
-    }),
   ],
 });
