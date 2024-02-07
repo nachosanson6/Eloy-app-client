f24ff991564004b71607cc6033f838d5ee257e5e
nachosanson6 <nachosanson6@gmail.com>
2024-02-07 13:48:54 +0100

    e

--- src/components/PhotoModal/PhotoModal.jsx
+++ src/components/PhotoModal/PhotoModal.jsx
@@ -3,12 +3,14 @@ import "./PhotoModal.css";
 
 const PhotoModal = ({ show, onClose, imageUrl }) => {
     if (!show) return null;
-    console.log(imageUrl)
+
     return (
         <div className="modal-overlay" onClick={onClose}>
             <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-                <span className="close" onClick={onClose}>X</span>
                 <img src={imageUrl} alt="" />
+                <div className="closeButton">
+                    <span className="close" onClick={onClose}>X</span>
+                </div>
             </div>
         </div>
     );
