2ae30f9b9017d2f7135df5945be4e615ef111068
nachosanson6 <nachosanson6@gmail.com>
2024-02-11 19:28:47 +0100

    w

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -15,12 +15,18 @@ const About = () => {
                 <div className="text">
                     <h4 className="artistName"> Eloy Sansón Kauffman</h4>
                     <p className="artistDescription">
-                        For athletes, high altitude produces two contradictory effects on performance. For explosive events
-                        Physiological respiration involves the mechanisms that ensure that the composition of the functional
-                        The long barrow was built on land previously inhabited in the Mesolithic period. It consisted of a s
-                        Physical space is often conceived in three linear dimensions, although modern physicists usually con
-                        In the eighteenth century the German philosopher Immanuel Kant developed a theory of knowledge in wh
-                        Maxwell's equations—the foundation of classical electromagnetism—describe light as a wave that moves
+                        Eloy inicia su vena artística cuando se prejubila en 2013. Después de un largo descanso, se dio cuenta que los días se hacían largos si no tenía una ocupación y se preguntó: ¿Y ahora qué hago?
+                        <br /> <br />
+                        Estas eran las alternativas, a saber: <br />
+                        - Buscar a otras personas para jugar al dominó o a la petanca. <br />
+                        - ⁠Sentarse en un banco a ver cómo construyen un edificio. <br />
+                        - ⁠Buscar alguna afición que le llenara y le mantuviera ocupado. <br /> <br />
+                        Eligió esta última y se puso a pintar siguiendo algunos tutoriales. Al cabo de un tiempo empezó con las esculturas y la bisutería y, desde entonces, el artista no ha parado de hacer obras.
+
+                        ¿Qué materiales utiliza?
+                        En las pinturas principalmente acrílico con pinceles y espátulas. En escultura, madera, cuerda, pintura y arcilla. En bisutería cualquier cosa vale: arcilla polimerica, pintura, tuercas, tornillos, etc.
+                        <br /> <br />
+                        Eloy ha encontrado una afición artística que le mantiene distraído, le gusta y disfruta realizándola. Le da una gran satisfacción.
                     </p>
                     <button onClick={() => { setShowContactModal(true) }} className='contactButton'>Contactar con Eloy</button>
                 </div>
