3116912d6e263758d63abeb4411e86d06874385f
nachosanson6 <nachosanson6@gmail.com>
2024-02-17 10:16:20 +0100

    q

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -2,9 +2,11 @@ import Spinner from 'react-bootstrap/Spinner';
 import './Loading.css'
 const Loading = () => {
     return (
-        <Spinner className='spinner' animation="border" role="status">
-            <span className="visually-hidden">Loading...</span>
-        </Spinner>
+        <div className="loading">
+            <Spinner className='spinner' animation="border" role="status">
+                <span className="visually-hidden">Loading...</span>
+            </Spinner>
+        </div>
     );
 }
 
